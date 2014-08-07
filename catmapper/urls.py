936eadcbd6f62369ef6cbe7f60270cfaf2fa2ce0
Adam Robak <adamr@wikia-inc.com>
2014-08-07 16:27:09 +0200

    s

--- catmapper/urls.py
+++ catmapper/urls.py
@@ -7,7 +7,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'catmapper.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^', 'map.views.main', name='main'),
+    # url(r'^', 'map.views.main', name='main'),
+    url(r'^(?P<wiki_id>\d+)/save/?', 'map.views.save', name='save'),
+    url(r'^(?P<wiki_id>\d+)/?', 'map.views.main', name='main'),
+
 
     url(r'^admin/', include(admin.site.urls)),
 )
