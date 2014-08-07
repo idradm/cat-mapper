00f20be2ec7a1e7fb840f5a76c5086dcb3b56e42
Adam Robak <adamr@wikia-inc.com>
2014-08-07 13:47:45 +0200

    hello world

--- catmapper/urls.py
+++ catmapper/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'catmapper.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^', 'map.views.main', name='main'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
